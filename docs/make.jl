b89b083e9c7f50c69374b6bfeca22f468db51cf3
Bíma <bima@merck.com>
2023-01-22 03:58:23 +0100

    add workflows

--- docs/make.jl
+++ docs/make.jl
@@ -2,8 +2,6 @@ using Documenter, DocumenterMarkdown
 using ReactiveDynamics
 
 makedocs(format = Documenter.HTML(prettyurls = false, edit_link = nothing),
-         sitename = "ReactiveDynamics.jl API Documentation",
-         build = "build_html", pages = ["index.md"])
+         sitename = "ReactiveDynamics.jl API Documentation", pages = ["index.md"])
 
-makedocs(format = Markdown(), sitename = "ReactiveDynamics.jl API Documentation",
-         build = "build_md", pages = ["index.md"])
+deploydocs(repo = "github.com/Merck/ReactiveDynamics.jl.git")
