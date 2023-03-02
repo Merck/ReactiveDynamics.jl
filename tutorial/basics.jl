4399f2899079fcb4c7808aa44c6b986631d59813
slwu89 <10673535+slwu89@users.noreply.github.com>
2023-03-01 17:33:51 -0800

    debugging

--- tutorial/basics.jl
+++ tutorial/basics.jl
@@ -7,3 +7,13 @@ end
 acs = @ReactionNetwork begin
     1.0, X ⟺ Y, name=>"transition1"
 end
+
+@prob_init acs X=10 Y=20
+@prob_params acs
+@prob_meta acs tspan=250 dt=0.1
+
+prob = @problematize acs
+
+# sol = @solve prob trajectories=50
+
+ReactiveDynamics.solve(prob)
