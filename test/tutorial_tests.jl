90e413c3b9e9844bcf74e5daa2a9ce5dd97d0ade
slwu89 <10673535+slwu89@users.noreply.github.com>
2023-10-06 10:32:23 -0700

    accidental commenting

--- test/tutorial_tests.jl
+++ test/tutorial_tests.jl
@@ -1,8 +1,8 @@
 using ReactiveDynamics
 
-@safetestset "example" "../tutorial/example.jl"
-@safetestset "joins" "../tutorial/joins/joins.jl"
-@safetestset "loadsave" "../tutorial/loadsave/loadsave.jl"
+@safeinclude "example" "../tutorial/example.jl"
+@safeinclude "joins" "../tutorial/joins/joins.jl"
+@safeinclude "loadsave" "../tutorial/loadsave/loadsave.jl"
 # @safeinclude "optimize" "../tutorial/optimize/optimize.jl"
 # @safeinclude "solution wrap" "../tutorial/optimize/optimize_custom.jl"
-@safetestset "toy pharma model" "../tutorial/toy_pharma_model.jl"
+@safeinclude "toy pharma model" "../tutorial/toy_pharma_model.jl"
